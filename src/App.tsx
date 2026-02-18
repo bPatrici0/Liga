41b1f321ca5a38d25720af6f15b3c2fe24bd3b19
bPatrici0 <b.patricio.ing@gmail.com>
2026-02-17 20:57:39 -0600

    implementacion de colores neon y reicinio del torneo

--- src/App.tsx
+++ src/App.tsx
@@ -283,7 +283,7 @@ function App() {
   return (
     <div style={{ padding: '2rem', maxWidth: '1200px', margin: '0 auto' }}>
       <header style={{ marginBottom: '3rem', textAlign: 'center' }}>
-        <h1>Liga Estelar</h1>
+        <h1>League of Legends</h1>
         <p style={{ color: 'var(--text-dim)' }}>Gestiona tus equipos y crea sorteos épicos</p>
       </header>
 
